aec6cce0dc2fe6cb20815e094c29dc5242385870
Amka <Aminhassen1470@gmail.com>
2022-12-27 20:41:54 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Attributions from './lib/Attributions.svelte';
+</script>
+
+<h1>EasyBank Landing Page Manager</h1>
+
+<Attributions />
+
+<style lang="scss">
+	// import variables
+	@use './sass/lib/' as *;
+
+	h1 {
+		color: $lime-green;
+		text-align: center;
+		padding-bottom: 5rem;
+	}
+</style>
---
+++ src/lib/Attributions.svelte
@@ -0,0 +1,25 @@
+<div class="attribution">
+	Challenge by
+	<a
+		href="https://www.frontendmentor.io?ref=challenge"
+		target="_blank"
+		rel="noreferrer"
+	>
+		Frontend Mentor
+	</a>. Coded by
+	<a href="https://github.com/AmkaE" target="_blank" rel="noreferrer">AmkaE</a>.
+</div>
+
+<style lang="scss">
+	// import variables
+	@use '../sass/lib/' as *;
+
+	.attribution {
+		font-size: 11px;
+		text-align: center;
+	}
+
+	.attribution a {
+		color: $attribution-color;
+	}
+</style>
